56cedfa55f41255f2dc4ece5404a15b755a68048
Gibloor <gibloor@yandex.ru>
2023-07-11 13:36:15 +0200

    added eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": ["**/*.ts", "**/*.tsx"],
+            "parserOptions": {
+              "project": "./tsconfig.json"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
